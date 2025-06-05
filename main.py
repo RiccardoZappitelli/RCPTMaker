4ea6febefdde70fbe250f32caec17de031cf8254
Ricc4rdo0107 <pythonisyeah@gmail.com>
2025-06-05 10:03:14 +0200

    Added ngrok token config and autosaving

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from shutil import rmtree
 import customtkinter as ctk
 from threading import Thread
 from os import chdir, listdir
-from os.path import isdir, join, abspath
 from subprocess import Popen, PIPE, STDOUT
+from os.path import isdir, join, abspath, isfile
 
 repo_name = abspath("RCPepTelegram")
 venv_path = abspath("venv")
@@ -19,7 +19,7 @@ class GUI:
         self.root = ctk.CTk()
         self.root.resizable(False, False)
         self.root.columnconfigure(1, weight=2)
-        self.root.rowconfigure(2, weight=10)
+        self.root.rowconfigure(3, weight=10)
 
         self.chatidentry = ctk.CTkEntry(self.root, placeholder_text="YOUR CHAT ID HERE")
         self.chatidentry.grid(row=0, column=0, padx=12, pady=12, sticky="nsew")
@@ -27,16 +27,33 @@ class GUI:
         self.bottokenentry = ctk.CTkEntry(self.root, placeholder_text="YOUR BOT TOKEN HERE")
         self.bottokenentry.grid(row=0, column=1, padx=12, pady=12, sticky="nsew")
 
-        self.compile_button = ctk.CTkButton(self.root, text="COMPILE", command=Thread(target=self.compile_pep).start)
-        self.compile_button.grid(row=1, column=0, sticky="nsew", columnspan=2, padx=12, pady=0)
+        self.ngroktokenentry = ctk.CTkEntry(self.root, placeholder_text="YOUR NGROK TOKEN HERE")
+        self.ngroktokenentry.grid(row=1, column=0, padx=12, pady=12, sticky="nsew", columnspan=2)
+
+        self.compile_button = ctk.CTkButton(self.root, text="COMPILE", command=self.compiling_thread)
+        self.compile_button.grid(row=2, column=0, sticky="nsew", columnspan=2, padx=12, pady=0)
 
         self.output = ctk.CTkTextbox(self.root)
-        self.output.grid(row=2, column=0, columnspan=2, padx=12, pady=12, sticky="nsew")
+        self.output.grid(row=3, column=0, columnspan=2, padx=12, pady=12, sticky="nsew")
         self.output.configure(state="disabled")
         self.output.tag_config("green", foreground="green")
+        self.output.tag_config("red", foreground="red")
 
         self.root.geometry("500x500")
+        authfilename = join(repo_name, "auth.json")
+        if isfile(authfilename):
+            with open(authfilename,  "r") as authfile:
+                var = json.load(authfile)
+                token = var["token"].strip()
+                chatid = var["chatid"]
+                ngrok_token = var["ngrok_token"].strip()
+                self.bottokenentry.insert(0, token)
+                self.chatidentry.insert(0, chatid)
+                self.ngroktokenentry.insert(0, ngrok_token)
         self.root.mainloop()
+    
+    def compiling_thread(self) -> None:
+        Thread(target=self.compile_pep).start()
 
     def change_button_text(self, text: str) -> None:
         self.compile_button.configure(text=text)
@@ -56,7 +73,10 @@ class GUI:
         return self.bottokenentry.get()
 
     def getchatid(self) -> int:
-        return int(self.chatidentry.get())
+        return self.chatidentry.get()
+
+    def getngroktoken(self) -> str:
+        return self.ngroktokenentry.get()
 
     def enable_all(self) -> None:
         for w in (self.bottokenentry, self.chatidentry, self.compile_button):
@@ -71,6 +91,11 @@ class GUI:
         self.disable_all()
         token = self.gettoken()
         chatid = self.getchatid()
+        ngrok_token = self.getngroktoken()
+        if not all((token, chatid)):
+            self.writetextbox("You must have a chatid and a bottoken.", tag="red")
+            self.enable_all()
+            return
         try:
             bot = Bot(token)
         except:
@@ -79,7 +104,8 @@ class GUI:
             json.dump(
                 {
                     "token":token,
-                    "chatid":chatid
+                    "chatid":int(chatid),
+                    "ngrok_token":ngrok_token
                 }, authfile)
         self.change_button_text("CHECKING REQUIREMENTS")
         self.run_command(requirements_command)
